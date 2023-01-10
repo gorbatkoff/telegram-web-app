87f3e245f61d55a95dd6c846b507e6356bf176f8
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-10 14:11:12 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { React, useEffect } from 'react';
 
 import './App.css';
+import Header from './components/Header/Header';
 import { useTelegram } from './hooks/useTelegram';
 
 
 function App() {
 
-  const {onToggleButton, tg} = useTelegram();
+  const { onToggleButton, tg } = useTelegram();
 
   useEffect(() => {
     tg.ready()
@@ -14,6 +15,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
