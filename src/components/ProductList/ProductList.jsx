f7e60cbe877007208d80a83dd983e0167d2bcc5a
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-10 14:24:45 +0300

    Initial commit

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './ProductList.module.css'
+
+function ProductList() {
+    return (
+        <div>ProductList</div>
+    )
+}
+
+export default ProductList
