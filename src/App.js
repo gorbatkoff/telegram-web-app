2f5dae92dac305dcad0aa6bce61d942b7b849fc3
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-10 13:08:51 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { React, useEffect } from 'react';
+
 import './App.css';
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+
+  useEffect(() => {
+    tg.ready()
+  }, [])
+
+
+  const onClose = () => {
+    tg.close()
+  }
+
   return (
     <div className="App">
+      <h1>work</h1>
+      <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
