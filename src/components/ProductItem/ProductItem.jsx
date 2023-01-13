7faa3a2ac10ddbf47ee0e6d857cfef798ff1d4c9
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-13 14:53:20 +0300

    Init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -5,6 +5,7 @@ import styles from './ProductItem.module.css';
 
 import PersonIcon from '../../images/PersonIcon.svg';
 import Hot from '../../images/HotProduct.svg';
+import { Link } from 'react-router-dom';
 
 
 function ProductItem({ product, className, onAdd }) {
@@ -14,24 +15,27 @@ function ProductItem({ product, className, onAdd }) {
     }
 
     return (
-        <div className={styles.product + " " + className}>
-            <div className={styles.img}>
-                <img src={product.imgLink} alt="" height="100%" width="100%" />
-                <img src={Hot} alt="" className={styles.hot}/>
-            </div>
+        <Link to={`/products/${product.id}`}>
 
-            <div className={styles.title}>
-                {product.title}
-            </div>
+            <div className={styles.product + " " + className}>
+                <div className={styles.img}>
+                    <img src={product.imgLink} alt="" height="100%" width="100%" />
+                    <img src={Hot} alt="" className={styles.hot} />
+                </div>
 
-            <button className={styles['add-btn']}
-                onClick={onAddHandler}
-            >
-                <div className={styles.price}>
-                    <img src={PersonIcon} alt="" />
-                    <span>{product.price}</span>
-                </div></button>
-        </div>
+                <div className={styles.title}>
+                    {product.title}
+                </div>
+
+                <button className={styles['add-btn']}
+                    onClick={onAddHandler}
+                >
+                    <div className={styles.price}>
+                        <img src={PersonIcon} alt="" />
+                        <span>{product.price}</span>
+                    </div></button>
+            </div>
+        </Link>
     )
 }
 
